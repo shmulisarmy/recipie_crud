d5dd9e116619b9d102c43ac9ae01ebaa9c7bee09
shmuli <shmulikeller@gmail.com>
2024-05-09 23:58:36 -0400

    started_setting_up_templates

--- recipies/routes.py
+++ recipies/routes.py
@@ -3,6 +3,17 @@ from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from typing import List, Optional
+from fastapi.templating import Jinja2Templates
 
 
 from .dadabase_interactions import get_recipies
+
+router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+
+
+@router.get("/recipies")
+def get_recipies(request: Request):
+    recipies = get_recipies()
+    return 
